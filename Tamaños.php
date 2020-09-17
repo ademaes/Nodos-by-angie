45db523ae38979ce7f3ebc059d4756d12e76ed6c
ademaes <71338904+ademaes@users.noreply.github.com>
2020-09-17 08:55:26 -0500

    Create Tamaños.php

---
+++ Tamaños.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title> Tamaños </title>
+    <link rel="stylesheet" type="text/css" href="style.css">   
+    
+</head>
+<body style="background-color: #F0FAFF;">
+    <div class="logo">
+        Nodos By 
+        Angie
+    </div>
+    <div class="titulo">
+        Elije el tamaño de tu manilla
+    </div>
+   
+   <div id="subtamaño">
+        Cada tamaño tiene diseños distintos
+    </div>
+    <div id="boxpequeña">
+        <p>CHIQUITA</p>
+    </div>
+    <div id="boxmediana">
+        <p>MEDIANA</p>
+    </div>
+    <div id="boxgrande">
+        <p>
+        GRANDE
+        </p>
+    </div>
+</body>
+</html>
